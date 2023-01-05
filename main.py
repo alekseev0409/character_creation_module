321510300e60052548dff54dcc4b596c2166af3e
Алексей Алексеев <alekseev0409@gmail.com>
2023-01-05 17:22:27 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
+def attack(char_name: str, char_class: str) -> str:
+    """Функция атаки."""
+
+def defence(char_name: str, char_class: str) -> str:
+    """Функция защиты."""
+
+def special(char_name: str, char_class: str) -> str:
+    """Специальные навыки."""
+
+def start_training(char_name: str, char_class: str) -> str:
+    """Старт тренировки."""
+
+def choice_char_class() -> str:
+    """Выбор персонажа."""
+
+def main() -> None:
+    run_screensaver()
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class: str = choice_char_class()
+    print(start_training(char_name, char_class))
+
+main() 
 
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
@@ -80,7 +110,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -89,7 +120,4 @@ def main():
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
-    print(start_training(char_name, char_class))
-
-
-main()
+    print(start_training(char_name, char_class)) 
